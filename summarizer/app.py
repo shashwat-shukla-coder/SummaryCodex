30095fce3f38687557ac913ff1ee3fe0ad7bb1a9
shashwat shukla <shashwatshukla15march@gmail.com>
2025-07-18 23:22:21 +0530

    fixed the routes

--- summarizer/app.py
+++ summarizer/app.py
@@ -107,5 +107,6 @@ def abstractive():
     except Exception as e:
         return jsonify({'error': 'Failed to summarize the text.', 'details': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
