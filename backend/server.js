fa22bcbb963f29394129f5bc41561fcb25c976de
shashwat shukla <shashwatshukla15march@gmail.com>
2025-07-18 22:55:17 +0530

    minor fix for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  }
+  })
+} 
+else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
 }
-
-
 
 //------------
 app.use(notFound);
