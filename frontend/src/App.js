d15e2ecdee9799f9d40d05e6a1f862f460a40ddd
shashwat shukla <shashwatshukla15march@gmail.com>
2025-07-22 22:38:21 +0530

    implemented funtiom to hit the health route of api to wake it up when frontend is rendered to shorten latency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,31 @@ import MyNotes from "./screens/Mynotes/MyNotes";
 import CreateNote from "./screens/NewNote/CreateNote";
 import SingleNote from "./screens/NewNote/SingleNote";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SummaryNote from "./screens/NewNote/SummaryNote";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
-// classname instead of class as in html
-// fragmentation
 function App() {
   const [search, setSearch] = useState("");
+
+  const { userInfo } = useSelector((state) => state.userLogin);
+
+  const wakeUpRenderAPI = async () => {
+    try {
+      const res = await axios.get("https://summarizer-api-qydn.onrender.com/");
+      console.log("Render API awake:", res.data);
+    } catch (error) {
+      console.error("Wake-up failed:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      wakeUpRenderAPI();
+    }
+  }, [userInfo]);
+
   return (
     <>
       <BrowserRouter>
